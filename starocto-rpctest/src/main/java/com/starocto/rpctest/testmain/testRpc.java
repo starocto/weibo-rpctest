2252499c0c3498206273a375ff29d84aa86c56bd
zhangjianxun <zhangjianxun@meituan.com>
2018-11-04 10:33:35 +0800

    1

---
+++ starocto-rpctest/src/main/java/com/starocto/rpctest/testmain/testRpc.java
@@ -0,0 +1,27 @@
+package com.starocto.rpctest.testmain;
+
+import com.starocto.dao.api.service.WeiboQueryService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Author : zjx@.com
+ * Date   : 2018/11/4
+ * Time   : 10:20
+ * ---------------------------------------
+ * Desc   :
+ */
+public class testRpc {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dao-api.xml");
+        WeiboQueryService weiboQueryServiceImpl = (WeiboQueryService) context.getBean("weiboQueryServiceImpl");
+        System.out.println(weiboQueryServiceImpl.queryUserRegisterInfo("fuermoxun"));
+        System.out.println(weiboQueryServiceImpl.queryUserInfo(1));
+        System.out.println(weiboQueryServiceImpl.queryUserOthers(1));
+        System.out.println(weiboQueryServiceImpl.queryUserFollow(1));
+        System.out.println(weiboQueryServiceImpl.queryFans(2));
+        System.out.println(weiboQueryServiceImpl.queryBlogInfo(1));
+        System.out.println(weiboQueryServiceImpl.queryBlogCount(1));
+        System.out.println(weiboQueryServiceImpl.queryCommentToBlog(1));
+    }
+}
